6348f1c48dd3c242f46f5b05f9761da14cebdd2f
Jae-Hyeon Bae <cs.newcs0@gmail.com>
2024-11-14 19:23:11 +0900

    checkCpu 완성

--- project/new/server/gameData.go
+++ project/new/server/gameData.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type Game struct {
@@ -24,7 +25,8 @@ type Game struct {
 }
 
 type GameFinder struct {
-	Games           []Game
+	Games []Game
+	// apiSession      string
 	apiGetCpuName   string
 	apiGetGpuName   string
 	apiGetMedianUrl string
@@ -51,8 +53,21 @@ func NewGameFinder() GameFinder {
 	gameFinder.apiGetCpuName = "https://www.3dmark.com/proxycon/ajax/search/cpuname?term=%s"
 	gameFinder.apiGetGpuName = "https://www.3dmark.com/proxycon/ajax/search/gpuname?term=%s"
 	gameFinder.apiGetMedianUrl = "https://www.3dmark.com/proxycon/ajax/medianscore?test=fs%%20P&cpuId=%s&gpuId=%s&gpuCount=1&gpuType=ALL&deviceType=ALL&storageModel=ALL&showRamDisks=false&memoryChannels=0&country=&scoreType=%s&hofMode=false&showInvalidResults=false&freeParams=&minGpuCoreClock=&maxGpuCoreClock=&minGpuMemClock=&maxGpuMemClock=&minCpuClock=&maxCpuClock="
+	// gameFinder.apiSession = getSession()
 	return gameFinder
 }
+
+// func getSession() string { //Session을 주지 않으면 proxycon의 median부분이 동작하지 않음(406 Not Acceptable)
+// 	url := "https://www.3dmark.com/search"
+// 	result, _ := http.Get(url)
+// 	for _, value := range result.Cookies() {
+// 		if value.Name == "FM_PROXYCON_SESSION" {
+// 			fmt.Println(value.Value)
+// 			return value.Value
+// 		}
+// 	}
+// 	return ""
+// }
 
 func (gameFinder *GameFinder) GetGameList() string {
 	str := ""
@@ -72,8 +87,12 @@ func (gameFinder *GameFinder) GetGame(name string) Game {
 	return Game{}
 }
 func (gameFinder *GameFinder) CheckCpuRun(cpuScore float64, game Game) int { //0: Not, 1: Bigger than minimum, 2: Yes
-
-	return 0
+	if cpuScore < float64(game.MinimumCPUScore) {
+		return 0
+	} else if cpuScore < float64(game.RecommendCPUScore) {
+		return 1
+	}
+	return 2
 }
 
 func (gameFinder *GameFinder) GetCpuScore(cpuName string) float64 {
@@ -82,29 +101,36 @@ func (gameFinder *GameFinder) GetCpuScore(cpuName string) float64 {
 	fmt.Println(url)
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0")
+	// sessionCookie := http.Cookie{
+	// 	Name:  "FM_PROXYCON_SESSION",
+	// 	Value: gameFinder.apiSession,
+	// }
+	// req.Header.Add("Cookie", sessionCookie.String())
+	req.Header.Add("Accept", "application/json, text/javascript, */*; q=0.01")
 	client := &http.Client{}
 	result, _ := client.Do(req)
 	// result, _ := http.Get(url)
-	var data map[string]interface{}
+	var data map[string]float64
 
 	defer result.Body.Close()
 	str, _ := io.ReadAll(result.Body)
 	fmt.Printf("%s\n", str)
-	json.NewDecoder(result.Body).Decode(&data)
+	json.Unmarshal(str, &data)
 	for key, value := range data {
 		fmt.Println(key, value)
 	}
-	return 0
+	return data["median"]
 }
 
 func (gameFinder *GameFinder) getCpuId(cpuName string) int {
 	url := fmt.Sprintf(gameFinder.apiGetCpuName, cpuName)
-	println(url)
+	url = strings.ReplaceAll(url, " ", "%20")
+	fmt.Println(url)
 	result, _ := http.Get(url)
 	var data []Cpu3dMark
 	str, _ := io.ReadAll(result.Body)
+	fmt.Printf("%s\n", str)
 	json.Unmarshal(str, &data)
-	fmt.Println("===========================DATA===========================")
 	for _, value := range data {
 		if value.Label == cpuName {
 			id, _ := strconv.Atoi(value.Id)
--- project/new/server/main.go
+++ project/new/server/main.go
@@ -11,7 +11,6 @@ var gameFinder GameFinder
 
 func main() {
 	gameFinder = NewGameFinder()
-	fmt.Println(gameFinder.GetCpuScore("i7-7700HQ"))
 	server := echo.New()
 	server.GET("/", func(c echo.Context) error {
 		return c.String(200, "hi")
@@ -27,14 +26,18 @@ func checkCpu(c echo.Context) error {
 	name := c.QueryParam("cpu")
 	gameId := c.Param("game")
 	if name == "" {
-		return c.String(400, "-1, No cpu name")
+		return c.String(400, "-1\nNo cpu name")
+	}
+	id, err := strconv.Atoi(gameId)
+	if err != nil || id >= len(gameFinder.Games) || id < 0 {
+		return c.String(400, "-1\nInvalid game id")
 	}
 	cpuScore := gameFinder.GetCpuScore(name)
-	id, err := strconv.Atoi(gameId)
 	game := gameFinder.Games[id]
-	if err != nil {
-		return c.String(400, "-1, Invalid game id")
-	}
 	result := gameFinder.CheckCpuRun(cpuScore, game)
-	return c.String(200, fmt.Sprintf("%d %s %s %s %dGB %s %s %dGB", result, name, game.MinimumCPU, game.MinimumGPU, game.MinimumMem, game.RecommendCPU, game.RecommendGPU, game.RecommendMem))
+	return c.String(200, fmt.Sprintf("%d\n%s\n%s,%s,%dGB\n%s,%s,%dGB", result, name, game.MinimumCPU, game.MinimumGPU, game.MinimumMem, game.RecommendCPU, game.RecommendGPU, game.RecommendMem))
+	//첫째 줄: 실행 가능 여부(0: 불가능, 1: 최소 사양 이상, 2: 권장 사양 이상)
+	//둘째 줄: Request의 성능(CPU)
+	//셋째 줄: 최소 사양
+	//넷째 줄: 권장 사양
 }
